b0c0ec9966484eafccbf022792b1e1b2f61305d5
Chris Minosora <mino0027@algonquinlive.com>
2024-10-14 10:53:32 -0400

    tshirts js should be complete

--- t-shirts.js
+++ t-shirts.js
@@ -82,7 +82,7 @@ function TShirt({ tshirt }) {
       <img src={tshirt.image} alt={tshirt.title} />
       <p>Price: ${tshirt.price}</p> 
       <p>Stock: {stock > 0 ? stock : "Out of Stock, sorry!"}</p> 
-      {stock > 0 && ( // stock > 0 means that if the stock is greater than 0, the dropdown will appear, if not, the out of stock message will show
+      {stock > 0 && ( // stock > 0 means that if the stock is greater than 0, the dropdown will appear, if not, the out of stock message will show. this is done by && which is a logical operator that only returns the second value if the first value is true. in this case, if stock is greater than 0, the dropdown will appear. if not, the out of stock message will appear
         //what <> does is it allows a return of multiple elements without having to wrap them in a div, as it's a shorthand for a div
         <> 
           <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
